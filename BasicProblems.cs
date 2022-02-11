4b18b5e238741658e623196077feaf713fb33666
ifratmitul <ifrat.ikhtear@gmail.com>
2022-02-11 13:31:08 +0600

    LeetCode Problems

---
+++ BasicProblems.cs
@@ -0,0 +1,31 @@
+namespace LeetCode
+{
+    public class BasicProblems
+    {
+        public int RemoveDuplicates(int[] num)
+        {
+            int i, j = 0;
+
+            for (i = 1; i < num.Length; i++)
+            {
+                if (num[i] != num[j])
+                {
+                    num[++j] = num[i];
+                }
+            }
+
+            return j + 1;
+        }
+
+
+        //SubArray
+        public int MaxSubArray()
+        {
+            return 0;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿
+var call = new BasicProblems();
+int[] arr = { 1, 2, 2, 3, 3 };
+Console.WriteLine(call.RemoveDuplicates(arr));
+
